78c3f62d134805846c23e4b94c03f0ef46b865c9
nclongkk <nclongkk@gmail.com>
2023-06-18 22:49:46 +0700

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   const configService = app.get(ConfigService);
   const port = configService.get('PORT') || 3000;
   await app.listen(port);
